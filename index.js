6d53cd06435b4db3d1e5c24c913c905d69ba7afb
lucasRodrigues0 <lucasrodrigues119816@gmail.com>
2023-11-12 19:46:41 -0300

    conversão para async/await

--- index.js
+++ index.js
@@ -3,7 +3,29 @@ const app = express();
 const spotifyService = require("./spotifyService");
 const fetch = require('node-fetch');
 
-app.get('/playlists/:id', (req, res) => {
+app.get('/playlists/:id', async (req, res) => {
+    try {
+        const token = await spotifyService.getToken();
+        const response = await fetch(`https://api.spotify.com/v1/playlists/${req.params.id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+
+        if(response.status === 200) {
+            const data = await response.json();
+            res.send(data);
+        } else {
+            throw new Error('Error: ' + response.status);
+        }
+
+    } catch(error) {
+        res.status(500).send(error.message);
+    }
+})
+
+/* app.get('/playlists/:id', (req, res) => {
     spotifyService.getToken()
     .then((token) => {
         return fetch(`https://api.spotify.com/v1/playlists/${req.params.id}`, {
@@ -23,9 +45,9 @@ app.get('/playlists/:id', (req, res) => {
             res.status(500).send(error.message);
         })
     })
-})
+}) */
 
-app.get('/tracks/:id', (req, res) => {
+/* app.get('/tracks/:id', (req, res) => {
     spotifyService.getToken()
     .then((token) => {
         return fetch(`https://api.spotify.com/v1/tracks/${req.params.id}`, {
@@ -67,8 +89,8 @@ app.get('/playlists/user/:id', (req, res) => {
             res.status(500).send(error.message);
         })
     })
-})
+}) */
 
 app.listen(3000, () => {
     console.log('funcionando');
-})
+}) 
--- spotifyService.js
+++ spotifyService.js
@@ -6,43 +6,34 @@ const client_secret = credentials.client_secret;
 
 let extToken;
 
-getToken = () => {
+async function getToken () {
     if(extToken) {
-        return Promise.resolve(extToken); //retorna o token caso já exista
-    } 
-    else {
-        return new Promise((resolve, reject) => {
-
+        return extToken;
+    } else {
+        try {
             const requestBody = new URLSearchParams();
             requestBody.append('grant_type', 'client_credentials');
-
-            return fetch('https://accounts.spotify.com/api/token', { //faz um fetch do endpoint do token
+        
+            const response = await fetch('https://accounts.spotify.com/api/token', { //faz um fetch do endpoint do token
                 method: 'POST',
                 body: requestBody.toString(),
                 headers: {
                     'Authorization': 'Basic ' + new Buffer.from(`${client_id}:${client_secret}`).toString('base64'),
                     'Content-type': 'application/x-www-form-urlencoded'
                 }
-            })
-            .then(response => {
-                if(response.status === 200) {
-                    return response.json(); //retorna um json com a resposta
-                } else {
-                    throw new Error('Erro: código ' + response.status);
-                }                
-            })
-            .then(data => {
-                if(data.access_token) {
-                    extToken = data.access_token; //atribui o token gerado para a variável extToken
-                    resolve(extToken);
-                } else {
-                    throw new Error('Erro ao obter o token');
-                    reject(extToken);
-                }
             });
-        });
+
+            if(response.status === 200) {
+                const data = await response.json();
+                extToken = data.access_token;
+                return extToken;
+            }
+        } catch (error) {
+            throw new Error('error: ', error.message);
+        }
+                
     }
-};
+}
 
 module.exports = {
     getToken
