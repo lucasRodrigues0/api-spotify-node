bae9b75c945230b16864308d133e4b41daf9e04f
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2023-11-17 15:55:06 -0300

    reorganização

--- index.js
+++ index.js
@@ -23,73 +23,7 @@ app.get('/playlists/:id', async (req, res) => {
     } catch(error) {
         res.status(500).send(error.message);
     }
-})
-
-/* app.get('/playlists/:id', (req, res) => {
-    spotifyService.getToken()
-    .then((token) => {
-        return fetch(`https://api.spotify.com/v1/playlists/${req.params.id}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        }).then(response => {
-            if(response.status === 200) {
-                return response.json();
-            } else {
-                throw new Error('Erro: código ' + response.status);
-            }                
-        }).then(data => {
-            res.send(data);
-        }).catch(error => {
-            res.status(500).send(error.message);
-        })
-    })
-}) */
-
-/* app.get('/tracks/:id', (req, res) => {
-    spotifyService.getToken()
-    .then((token) => {
-        return fetch(`https://api.spotify.com/v1/tracks/${req.params.id}`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-        }).then(response => {
-            if(response.status === 200) {
-                return response.json();
-            } else {
-                throw new Error('Erro: código ' + response.status);
-            }
-        }).then(data => {
-            res.send(data);
-        }).catch(error => {
-            res.status(500).send(error.message);
-        })
-    })
-})
-
-app.get('/playlists/user/:id', (req, res) => {
-    spotifyService.getToken()
-    .then((token) => {
-        return fetch(`https://api.spotify.com/v1/users/${req.params.id}/playlists`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        }).then(response => {
-            if(response.status === 200) {
-                return response.json();
-            } else {
-                throw new Error('Erro: código ' + response.status);
-            }
-        }).then((data) => {
-            res.send(data);
-        }).catch(error => {
-            res.status(500).send(error.message);
-        })
-    })
-}) */
+});
 
 app.listen(3000, () => {
     console.log('funcionando');
